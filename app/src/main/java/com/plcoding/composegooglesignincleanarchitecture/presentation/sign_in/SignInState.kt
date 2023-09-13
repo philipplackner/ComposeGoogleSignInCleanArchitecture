17d2881f85a6bbafbd699276f1a903e9ae9fef7e
Amisha <bamisha814@gmail.com>
2023-09-13 15:35:43 +0530

    change

--- app/src/main/java/com/plcoding/composegooglesignincleanarchitecture/presentation/sign_in/SignInState.kt
+++
@@ -1,6 +0,0 @@
-package com.plcoding.composegooglesignincleanarchitecture.presentation.sign_in
-
-data class SignInState(
-    val isSignInSuccessful: Boolean = false,
-    val signInError: String? = null
-)
